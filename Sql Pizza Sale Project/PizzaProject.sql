bb1c11c82b38558cad9f158d58c59753cc5236dd
1432AbhayPandey <142674122+1432AbhayPandey@users.noreply.github.com>
2024-06-13 20:52:27 +0530

    Update PizzaProject.sql

--- Sql Pizza Sale Project/PizzaProject.sql
+++ Sql Pizza Sale Project/PizzaProject.sql
@@ -1,1 +1,22 @@
+create database pizzahut;
+use pizzahut;
 
+create table orders (
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key(order_id));
+
+select * from  orders;
+
+create table orders_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+order_time time not null,
+quantity int not null,
+primary key(order_details_id));
+
+alter table orders_details
+drop column order_time;
+select * from  orders;
